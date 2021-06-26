aa14c8cd5e1eca700e297bda230c3b4565c40294
alexcataguases <alexcataguases@hotmail.com>
2021-06-26 00:00:56 -0300

    comentarios no horme

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,16 @@
+/*
+
+https://capacitorjs.com/docs/apis/geolocation
+npm install @capacitor/geolocation
+ionic cap sync
+
+android->app->src->main->AndroidManifest.xml
+<uses-permission android:name="android.permission.ACCESS_COARSE_LOCATION" />
+<uses-permission android:name="android.permission.ACCESS_FINE_LOCATION" />
+<uses-feature android:name="android.hardware.location.gps" />
+
+*/
+
 import { Component } from '@angular/core';
 import { Geolocation } from '@capacitor/geolocation';
 
