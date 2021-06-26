f2bae1494a1ced7fcc3bc411ed0564863d3035ad
alexcataguases <alexcataguases@hotmail.com>
2021-06-25 23:57:42 -0300

    Primeiro comit do geolocation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() { }
 
+  async localizar() {
+    let coordinates = await Geolocation.getCurrentPosition();
+    alert('Current position:\n' + coordinates.coords.latitude+" \n "+coordinates.coords.longitude) ;
+  };
 }
+
+
